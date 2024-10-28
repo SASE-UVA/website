a6405ee7273e82f554e917d5b99ae77dc1384c57
jpark0504 <justinpark04@gmail.com>
2024-10-28 00:29:59 -0400

    small note on home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation'
 
 const About = () => {
     return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to SASE @ UVA</h1>
-            {/* Add events content here */}
+            <h2 className='text-2xl sm:text-4xl font-bold text-white'>
+                <TypeAnimation
+                sequence={[
+                    'This website is currently being built, come back later to see it complete!',
+                    1000,
+                ]}
+                speed={40}
+                repeat={Infinity}
+                />
+            </h2>
         </div>
     );
 };
