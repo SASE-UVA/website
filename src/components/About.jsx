acd0e3a6188dca4f09867b4c869eedfe12926136
jpark0504 <justinpark04@gmail.com>
2024-11-02 01:56:23 -0400

    updates to sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./About.css"
+import "./About.css";
 
 const About = () => {
     return (
@@ -7,7 +7,10 @@ const About = () => {
             <div className="Title">
                 <h1>About SASE @ UVA</h1>
             </div>
-            {/* Add about content here */}
+            <ul className="content-list">
+                <li>The Society of Asian Scientists and Engineers (SASE) was founded in November 2007 to help Asian heritage scientific and engineering professionals achieve their full potential.</li>
+                <li>After going dormant for the past couple of years at UVA, it has been revamped and brought back in the 2024-25 academic year!</li>
+            </ul>
         </about>
     );
 };
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import "./Events.css"
+import "./Events.css";
 
 const Events = () => {
     return (
         <events>
             <h1>Upcoming Events</h1>
             {/* Add events content here */}
+            <ul className="list">
+                <li>11/2 - CSA X UPC Full Moon Fest</li>
+                <li>11/4 - UVA Medical Entrepreneur Speaker Series</li>
+                <li>11/7 - Rush Hour Movie w/ NSBE</li>
+                <li>11/16 - Grateful Giving</li>
+                <li>11/21 - Last GBM + Potluck</li>
+            </ul>
         </events>
     );
 };
