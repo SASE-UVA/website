b1c7978ab3f8b027fbe6ab8ca58ee841854734a4
jpark0504 <justinpark04@gmail.com>
2024-11-14 23:39:20 -0500

    removed mail visibility

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -9,7 +9,7 @@ const Events = () => {
             </div>
             <div className="calendar">
                 <iframe
-                    src="https://calendar.google.com/calendar/embed?src=uvasaseevents%40gmail.com&ctz=America%2FNew_York"
+                    src="https://calendar.google.com/calendar/embed?src=uvasaseevents%40gmail.com&ctz=America%2FNew_York&showTitle=0"
                     style={{ border: 0, width: '100%', height: '600px' }}
                     title="Google Calendar">
                 </iframe>
